a001995d7a4032efff3c1529e06e173665c63583
cmeldrum123 <magicmeldrum@gmail.com>
2023-02-16 19:41:41 -0800

    asd

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,6 @@ package src;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello humans on this world!");
-        //Does it work?
-        // helloo this is a stupid comment
-        // comment - fred
-        // comment - Ben
-        // comment - christian
-        //hello again
+        
     }
 }
