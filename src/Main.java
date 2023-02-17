f40fc09706b8f3bfa5507092ea8df9c0e0a84d28
gamiv <vrajgami100@gmail.com>
2023-02-16 19:19:31 -0800

    i created re pository

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+package src;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello humans on this world!");
+        //Does it work?
+        // helloo this is a stupid comment
+        // comment - fred
+
+        //hello again
+    }
+}
---
+++ src/Suit.java
@@ -0,0 +1,15 @@
+package src;
+
+/**
+ * basics.src.Suit has values corresponding to a suit in cards
+ */
+public enum Suit {
+    CLUBS("C"), DIAMONDS("D"), HEARTS("H"), SPADES("S");
+    private final String symbol;
+    Suit(String s){
+        this.symbol = s;
+    }
+    public String toShortString(){
+        return symbol;
+    }
+}
