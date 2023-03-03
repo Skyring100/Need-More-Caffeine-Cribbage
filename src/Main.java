e39d329cec54c06e94e25e69363a9b3c9de1de96
cmeldrum123 <magicmeldrum@gmail.com>
2023-03-02 18:48:44 -0800

    asd

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,13 @@ package src;
 
 import java.util.ArrayList;
 
+import src.GUI.welcome_Frame;
+
 
 public class Main {
     public static void main(String[] args) {
     	//Game game = new Game();
+    	new welcome_Frame();
         
     }
 }
