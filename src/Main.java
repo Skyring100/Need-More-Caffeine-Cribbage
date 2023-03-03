402d12c2618287ad65aae20f3abd3dfa017d3c79
Skyring100 <skyolmsted22@gmail.com>
2023-03-02 18:53:51 -0800

    You cna new use a IgnoreTest class to test your own ideas without effecting

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,7 @@
 package src;
 
-import java.util.ArrayList;
-
-import src.GUI.welcome_Frame;
-
-
 public class Main {
     public static void main(String[] args) {
     	//Game game = new Game();
-    	new welcome_Frame();
-    	//hello
-        
     }
 }
