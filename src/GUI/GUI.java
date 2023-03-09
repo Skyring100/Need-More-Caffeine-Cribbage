9e1ba15acdf2b885a0c0f01612051254de59bed8
gamiv <vrajgami100@gmail.com>
2023-03-08 16:58:36 -0800

    I added a playing game screen for just user

--- src/GUI/GUI.java
+++ src/GUI/GUI.java
@@ -4,4 +4,7 @@ public class GUI {
     private static final int WINDOW_WIDTH = 1000;
     private static final int WINDOW_HEIGHT = 500;
 
+    public static void main(String[] args) {
+        new During_game();
+    }
 }
