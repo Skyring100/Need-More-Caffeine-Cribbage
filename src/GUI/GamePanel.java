cacd111d32b5cefc2195aea1f25b088173003d97
gamiv <vrajgami100@gmail.com>
2023-03-10 19:00:24 -0800

    I added bot panel

--- src/GUI/GamePanel.java
+++ src/GUI/GamePanel.java
@@ -2,11 +2,10 @@ package src.GUI;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class GamePanel extends JPanel{
     private int deckCount = 0, tableCount = 0;
+    private boolean bot_won= false;
     private JPanel handPanel, deckPanel, tablePanel, boat_panel;
     private JButton[] cardButtons;
 
@@ -26,35 +25,44 @@ public class GamePanel extends JPanel{
 
         boat_panel = new JPanel();
         boat_panel.setBackground(Color.ORANGE);
+        boat_panel.setLayout(new GridLayout(1,6));
+        JLabel[] cards = new JLabel[6];
+        for (int i = 0; i< 6 ; i++){
+            cards[i] = new JLabel();
+            cards[i].setIcon(new ImageIcon("blue.png"));
+
+            if (i < 2) {
+                deckPanel.add(cards[i]);
+            }else{
+                boat_panel.add(cards[i]);
+            }
+
+        }
+
 
         cardButtons = new JButton[6];
         for (int i = 0; i < 6; i++) {
-            cardButtons[i] = new JButton(new ImageIcon("club 2_resized.jpg") );
+            cardButtons[i] = new JButton(new ImageIcon("club 2_resized.jpg") );// getting the image from the user
+            int finalI = i;
             cardButtons[i].addActionListener(e -> {
                 JButton button = (JButton) e.getSource();
-                button.setEnabled(false);
+                int x = 0, y = 0;
 
                 if (deckCount < 2) {
-
+                    button.setEnabled(false);
                     deckPanel.add(button);
                     deckCount++;
                     validate();
                     repaint();
                 } else {
                     tableCount++;
+                    tablePanel.add(new JLabel(new ImageIcon("club 3_resized (1).jpg")));
+                    boat_panel.remove(0);
+                    move_component(x,y,tableCount,tablePanel);
                     tablePanel.add(button);
+                    move_component(x,y,tableCount,tablePanel);
                     validate();
                     repaint();
-
-                    int x = 0, y = 0;
-                    for (int i1 = 0; i1 < tableCount; i1++) {
-                        Component comp = tablePanel.getComponent(i1);
-                        comp.setLocation(x, y);
-                        comp.setSize(comp.getWidth(), comp.getHeight());
-                        x += 50;
-                        y += 50;
-                        comp.setLocation(x, y);
-                    }
                 }
             });
             handPanel.add(cardButtons[i]);
@@ -64,5 +72,15 @@ public class GamePanel extends JPanel{
         add(tablePanel, BorderLayout.CENTER);
         add(boat_panel,BorderLayout.NORTH);
     }
+    public void move_component(int locationX,int locationY, int count, JPanel panel){
+        for (int i1 = 0; i1 < count; i1++) {
+            Component comp = panel.getComponent(i1);
+            comp.setLocation(locationX, locationY);
+            comp.setSize(comp.getWidth(), comp.getHeight());
+            locationX += 100;
+            locationY += 100;
+            comp.setLocation(locationX, locationY);
+        }}
+
 
 }
