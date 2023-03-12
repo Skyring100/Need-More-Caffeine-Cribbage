2b816e82e6ab2119ae550c5980e7c5eef54c558a
Fred Brumovsky <freddiebrumovsky21@gmail.com>
2023-03-12 16:15:25 -0700

    ask vraj

--- src/GUI/WelcomePanel.java
+++ src/GUI/WelcomePanel.java
@@ -175,7 +175,12 @@ public class WelcomePanel extends JPanel implements ActionListener {
     }
     public static void main(String[] args) {
         JFrame frame = new JFrame();
+       frame.setSize(1000,1000);
+        frame.setPreferredSize(new Dimension(HEIGHT,WIDTH));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.add(new WelcomePanel());
+        frame.setLocationRelativeTo(null);
         frame.setVisible(true);
+    
     }
 }
