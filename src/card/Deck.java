75d0ed0b43789c65917620dfc252d7dd065274c8
Ben <ben.vogt1@gmail.com>
2023-03-12 15:29:58 -0700

    Added constructor, and method bodies to Deck class.

--- src/card/Deck.java
+++ src/card/Deck.java
@@ -1,32 +1,43 @@
 package src.card;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
 
 public class Deck {
     private ArrayList<Card> discarded;
-    private ArrayList<Card> cards;
-    public Deck(){
-        //add all cards to the deck
+    private ArrayList<Card> cards = new ArrayList<Card>(52);
+    public Deck() {
+        int index = 0;
+        for (Suit suit : Suit.values()) {
+            for (Rank rank : Rank.values()) {
+                cards.add(new Card(rank, suit));
+            }
+        }
+        shuffle();
     }
 
     /**
      * Shuffles the deck
      */
     public void shuffle(){
+        Collections.shuffle(cards);
     }
 
     /**
-     * draws a card from the deck
+     * draws a random card from the deck
      * @return the drawn card
      */
     public Card draw(){
-        //PLACE HOLDER, SWITCH THIS
-        return new Card(0);
+        Random random = new Random(0);
+        int randomNumber = random.nextInt(52 - 1 + 1) + 1;
+        cards.remove(randomNumber);
+        return cards.get(randomNumber);
     }
 
     /**
      * Shuffles the discard pile back into the deck
      */
     public void shuffleDiscard(){
-
+        Collections.shuffle(discarded);
     }
 }
