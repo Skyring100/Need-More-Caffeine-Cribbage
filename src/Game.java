7d4dbc76c2eb999f23add59e713e50b9eb03b3ac
Ben <ben.vogt1@gmail.com>
2023-03-15 19:06:37 -0700

    Fixed the enterName method, and added it to the main class so that the user is promtped before the game starts. Which then constructs the player with the name entered by the user.

--- src/Game.java
+++ src/Game.java
@@ -26,7 +26,6 @@ public class Game {
 	private Scanner input;
 	private String inputString;
 	private int inputNumber;
-	private String inputName;
 	/**
 	 * Creates a two game with two human players
 	 * @param one player one
@@ -74,7 +73,6 @@ public class Game {
 	 */
 	private void run(){
 		System.out.println("\nNew Round!\n");
-		enterName();
 		dealPlayers();
 		
 		System.out.println("\nDealing");
@@ -707,17 +705,6 @@ public class Game {
 		return peg15(list) + pegPairs(list) + pegStraight(list);
 	}
 
-	/**
-	 * Prompts the user to enter their name
-	 * Will only accept names between 1 and 20 in length
-	 */
-	private void enterName() {
-		System.out.println("Enter Name: ");
-		do {
-			inputName = input.nextLine();
-			player1.name = inputName;
-		} while ((inputName.length() > 1) && (inputName.length() < 20));
-	}
 
 	
 
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,26 @@
 package src;
+
+import java.util.Scanner;
+
 public class Main {
+    protected static String inputName;
+    public static Scanner input  = new Scanner(System.in);;
     public static void main(String[] args) {
-        new Game(new Player());
+        enterName();
+        new Game(new Player(inputName));
     }
+
+    /**
+     * Prompts the user to enter their name
+     * Will only accept names between 1 and 20 in length
+     */
+    private static String enterName() {
+        do {
+            System.out.print("Enter Name: ");
+            inputName = input.nextLine().trim();
+        } while (!((inputName.length() > 1) && (inputName.length() < 20)));
+        return inputName;
+    }
+
 }
+
