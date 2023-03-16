e17e77b52180ca2a45e490050d348498d7df510e
gamiv <vrajgami100@gmail.com>
2023-03-15 17:58:01 -0700

    I have add validity method to test the input

--- src/GUI/WelcomePanel.java
+++ src/GUI/WelcomePanel.java
@@ -191,11 +191,7 @@ public class WelcomePanel extends JPanel implements ActionListener {
             Player p2 = new Bot();
             //create a new thread for the game class
             javax.swing.SwingUtilities.invokeLater(()-> {
-                try {
                     gui.showGame(new Game(p1, p2));
-                } catch (InterruptedException ex) {
-                    throw new RuntimeException(ex);
-                }
             });
         }
 
